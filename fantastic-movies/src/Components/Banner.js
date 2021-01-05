ca9d3b857690db4c482c1c6674431e1374e82df1
jami raza <muhammadjmai45@gmail.com>
2021-01-05 12:49:04 +0500

    for the time being project is finalize

--- fantastic-movies/src/Components/Banner.js
+++ fantastic-movies/src/Components/Banner.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "../services/axios";
 import request from "../services/request";
 import "./Banner.css";
+import Youtube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
 const Banner = () => {
   const [banner, setBanner] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("")
 
   useEffect(() => {
     async function fetchData() {
@@ -23,7 +27,32 @@ const Banner = () => {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
 
+  const opts = {
+    height:"350",
+    width: "100%",
+    playerVars: {
+      autoplay: 1
+    }
+  }
+
+  const handleClick = (banner) => {
+    if(trailerUrl) {
+      setTrailerUrl("")
+    } else {
+      movieTrailer(banner?.name || "")
+      .then 
+        ((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"))
+        })
+       .catch 
+        ((error) => console.log(error))
+      
+    }
+  }
+
   return (
+    <>
     <header
       className="banner"
       style={{
@@ -37,7 +66,7 @@ const Banner = () => {
           {banner?.name || banner?.title || banner?.original_name}
         </h1>
         <div className="banner_buttons">
-          <button className="banner_button">Play</button>
+          <button className="banner_button" onClick={() => handleClick(banner)}>Play</button>
           <button className="banner_button">My List</button>
         </div>
         <h1 className="banner_description">
@@ -45,6 +74,8 @@ const Banner = () => {
       </div>
       <div className="banner_fadeBottom"></div>
     </header>
+    {trailerUrl && <Youtube videoId={trailerUrl} opts={opts}/>}
+    </>
   );
 };
 
--- fantastic-movies/src/Components/Row.js
+++ fantastic-movies/src/Components/Row.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "../services/axios";
 import './Row.css'
+import Youtube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // A snippet of code which runs based on a specific condition
   useEffect(() => {
@@ -19,20 +23,43 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
   }, [fetchUrl]);
 
   console.log(movies);
+  const opts = {
+    height: "350",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    }
+  }
 
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("")
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          //https://www.youtube.com/watch?v=XtMThy8QKqU&t=9400s
+        const urlParams = new URLSearchParams(new URL(url).search)
+        setTrailerUrl(urlParams.get("v"))
+        })
+        .catch((error) => console.log(error))
+    }
+  }
   return (
     <div className="row">
       <h2 className="row_title">{title}</h2>
       <div className="row_posters">
         {/* several row posters */}
-        {movies.map((movie, key) => (
+        {movies.map((movie) => (
           <img
             key={movie.id} className={`row_poster ${isLargeRow && "row_posterLarge"} `}
             src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts}/>}
     </div>
   );
 };
